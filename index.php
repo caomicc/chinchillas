c73add0c96bf22cfe30cc2a5bdde8022f55c0049
Cameron Omiccioli <caomicc@gmail.com>
2015-12-10 16:18:08 -0500

    woo

---
+++ index.php
@@ -0,0 +1,204 @@
+<?php
+require_once('mobile_device_detect.php');
+mobile_device_detect(true,true,true,true,true,true,true,'http://csci325.ricksphp.com/omiccioli/final/abstract.php',false);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8" />
+<title>PhoneGap with Splash and Icon</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="css/jquery.mobile.min.css">
+  <link rel="stylesheet" href="../../media/css/jquery.mobile.flatui.css" />
+
+<!--<script src="cordova.js"></script>-->
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery.mobile.min.js"></script>
+
+<style type="text/css">
+<!--
+img.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+button {
+	padding-top: 50px;
+	margin: 50px;
+}
+
+
+-->
+</style>
+
+</head>
+
+<body>
+
+<div data-role="page" data-theme="a" id="page1">
+
+
+  <div data-role="header" data-fullscreen="true">
+    <h1>Chinchillas!</h1>
+  </div>
+
+  <div data-role=content>
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#page1" class="ui-btn-active">Home</a></li>
+        <li><a href="#page2">Facts</a></li>
+        <li><a href="#page3">Gallery</a></li>
+        <li><a href="#page4">Care</a></li>
+      </ul>
+    </div><!-- /navbar -->
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;Let's learn about chinchillas! They're really cool.</p>
+
+    	
+    <div align="center">
+    	<img src="http://smallpetselect.com/wp-content/uploads/2012/10/1780793_706509676061158_465143270_n.jpg" alt="a logo"  class="center">
+    </div>
+  </div>
+
+  <div data-role="footer" data-position="fixed" data-fullscreen="true">
+      <h4>Chinchillas</h4>
+  </div>
+
+</div>
+
+<div data-role="page" data-theme="a" id="page2" data-title="Chinchilla Fun Facts" data-add-back-btn="true" >
+
+  <div data-role="header" data-fullscreen="true">
+    <h1>Fun Facts</h1>
+  </div>
+
+  <div data-role=content>
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#page1">Home</a></li>
+        <li><a href="#page2" class="ui-btn-active">Facts</a></li>
+        <li><a href="#page3">Gallery</a></li>
+        <li><a href="#page4">Care</a></li>
+      </ul>
+    </div><!-- /navbar -->
+    <h3>&nbsp;&nbsp;&nbsp;&nbsp;Chinchilla Fun Facts!</h3>
+
+    <ol>
+        <li>Chinchillas are nocturnal.</li>
+        <li>Teeth grow up to 12 inches a year!</li>
+        <li>Chinchillas don't shed fur.</li>
+        <li>Baby chinchillas are called kits.</li>
+        <li>Chinchillas can jump up to 6 feet in the air.</li>
+    </ol>
+
+  </div>
+
+  <div data-role="footer" data-position="fixed" data-fullscreen="true">
+      <h4>Chinchillas</h4>
+  </div>
+
+</div>
+
+<div data-role="page" data-theme="a" id="page3" data-title="Photos" data-add-back-btn="true" >
+
+  <div data-role="header" data-fullscreen="true">
+    <h1>Gallery</h1>
+  </div>
+
+  <div data-role=content>
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#page1">Home</a></li>
+        <li><a href="#page2">Facts</a></li>
+        <li><a href="#page3" class="ui-btn-active">Gallery</a></li>
+        <li><a href="#page4">Care</a></li>
+      </ul>
+    </div><!-- /navbar -->
+    <h3>&nbsp;&nbsp;&nbsp;&nbsp;Chinchilla Pictures</h3>
+      <div align="center">
+          <img src="http://smallpetselect.com/wp-content/uploads/2012/10/1780793_706509676061158_465143270_n.jpg" width="300"  class="center">
+          <img src="https://s-media-cache-ak0.pinimg.com/736x/8e/06/c8/8e06c84b09b87fa9110d061b7bc9d77f.jpg" width="300"  class="center">
+          <img src="http://justcuteanimals.com/wp-content/uploads/2013/02/cute-animals-chinchilla-standing-up-pics.jpg" width="300" class="center">
+          <img src="http://imgur.com/574Z6xi.jpg" width="300"  class="center">
+          <img src="http://www.desibucket.com/db2/01/26265/26265.jpg" width="300"  class="center">
+          <img src="http://chinformation.com/birthday%20chins.jpg" width="300"  class="center"><br><br><br><br>
+
+
+
+      </div>
+
+  </div>
+
+  <div data-role="footer" data-position="fixed" data-fullscreen="true">
+      <h4>Chinchillas</h4>
+  </div>
+
+</div>
+
+<div data-role="page" data-theme="a" id="page4" data-title="Care Information" data-add-back-btn="true" >
+
+  <div data-role="header" data-fullscreen="true">
+    <h1>Care</h1>
+  </div>
+
+  <div data-role=content>
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#page1">Home</a></li>
+        <li><a href="#page2">Facts</a></li>
+        <li><a href="#page3">Gallery</a></li>
+        <li><a href="#page4" class="ui-btn-active">Care</a></li>
+      </ul>
+    </div><!-- /navbar -->
+    <h3>&nbsp;&nbsp;&nbsp;&nbsp;Chinchilla Care</h3>
+      <div data-role="collapsibleset">
+          <div data-role="collapsible" data-collapsed="true">
+              <h3> 1. Get a Proper Cage</h3>
+              <p>Chinchillas should be kept in cages. Since they are extremely active, the larger the cage, the better. Tall and wide is the best, but tall is better than wide if you can't get both.
+              </p>
+              <ul>
+                  <li>Chinchillas love to jump, so multiple levels are advisable. Ferret Nation and Critter Nation models usually have multiple levels.</li>
+                  <li>Ensure the cage has a solid bottom as a chinchilla can get their feet caught in wire floors, and cause serious harm to themselves trying to get free, such as feet sores and broken legs.</li>
+                  <li>Ensure the cage bars are close enough together that the chinchilla can't stick his head through them. If his head fits through the bars, his whole body will fit through them and he will escape. He may seem too big, but don't be fooled. It is all the fur.</li>
+                  <li>If you choose a wire cage, ensure that there are no bits of wire poking out that your chinchilla could cut himself on.</li>
+              </ul>
+          </div>
+          <div data-role="collapsible">
+              <h3>2. Provide healthy bedding</h3>
+              <p>Never use cedar or pine shavings as bedding, as the phenols are very strong and cause serious respiratory, skin, coat and liver problems. They are also poisonous to chinchillas if they try to eat them. Try to only use kiln dried shavings or Carefresh. Watch if the chinchillas consume the Carefresh bedding. Take it out immediately if you notice them consuming it. You can replace the bedding with Aspen Shavings. .</p>
+              <p>If desired, provide a nesting box for your chinchilla. This should measure at least 50 cm (20 in) in length and 25 cm (10 in) width and height. The nesting box should be placed on the floor, not on any shelves because the chinchilla might throw it down.</p>
+          </div>
+          <div data-role="collapsible">
+              <h3>3. Provide proper food.</h3>
+              <p>Your chinchilla should always have a supply of good quality timothy hay or second cut local (you can feed them small amounts of alfalfa, but not too much because it could cause foaming at the mouth). Keep it in a hay rack or hanging wire basket to help keep it clean.</p>
+          </div>
+          <div data-role="collapsible">
+              <h3>4. Give your chinchilla a dust bath</h3>
+              <p>for 10-15 minutes anywhere from daily to twice a week. Too much will cause dryness. but too little will cause their coats to build up oils which can cause a disfiguring and deadly fungus to grow on their skin. </p>
+          </div>
+          <div data-role="collapsible">
+              <h3>5. Provide healthy bedding</h3>
+              <p>for 10-15 minutes anywhere from daily to twice a week. Too much will cause dryness. but too little will cause their coats to build up oils which can cause a disfiguring and deadly fungus to grow on their skin. </p>
+          </div>
+          <div data-role="collapsible">
+              <h3>6. Give them something to chew on!</h3>
+              <p> Give them something to chew on for dental health. Chinchillas' teeth can grow up to 12 inches a year. Purchase a special block of wood or pumice stone for your chinchilla to chew on.
+              </p>
+
+              <p>Provide your chinchilla with branches that can be climbed and gnawed on. Ensure that these branches are not cut from trees that have recently been sprayed with chemicals, or that are poisonous such as yew, laburnum and fresh pine. Recommended trees are sycamore and manzanita, they provide straight wood that is quite resistant to the chinchilla's sharp teeth. Apple tree branches are another good alternative that can be found quite easily. Unbleached toilet paper rolls are fun for them to chew on and also provide more fiber in their diet.</p>
+          </div>
+      </div><br><br>
+       &nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.wikihow.com/Care-for-Chinchillas">Source: WikiHow</a>
+
+  </div>
+
+  <div data-role="footer" data-position="fixed" data-fullscreen="true">
+    <h4>Chinchillas</h4>
+  </div>
+
+</div>
+
+</body>
+</html>
